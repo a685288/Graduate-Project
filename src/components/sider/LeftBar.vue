90dc8da8746c73c1eb84a515682e5103381ca515
a685288 <a685288@gmail.com>
2020-07-11 15:11:03 +0800

    0711 card style

---
+++ src/components/sider/LeftBar.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  CheckboxGroup.CheckboxGroup(lable='課程長度' )
+    h2 課程長度
+    Checkbox.box(label='0' border)
+      span 不到2小時
+    Checkbox.box(label='2' border)
+      span 2-10小時 
+    Checkbox.box(label='11' border)
+      span 11-20小時 
+    Checkbox.box(label='20' border)
+      span 20小時以上 
+</template>
+<script>
+export default {
+  name: "leftbar"
+};
+</script>
+<style lang='scss' scoped>
+.CheckboxGroup {
+  padding: 100px;
+  h2 {
+    line-height: 200%;
+  }
+  .box {
+    margin: 10px;
+    width: 90%;
+    span {
+      font-size: 16px;
+    }
+  }
+}
+</style>
--- src/views/dashboardPages/Allclass.vue
+++ src/views/dashboardPages/Allclass.vue
@@ -1,73 +1,98 @@
 <script>
-import LeftBar from '@/components/header/LeftBar.vue';
-import defaultClass from '@/assets/defaultClass.png';
-import topic from '@/assets/topic.png';
-import time from '@/assets/time.png';
+import LeftBar from "@/components/sider/LeftBar.vue";
+import defaultClass from "@/assets/defaultClass.png";
+import topic from "@/assets/topic.png";
+import time from "@/assets/time.png";
 export default {
-    name: 'allclass',
-    components: {
-        LeftBar
-    },
-    data() {
-        return {
-            defaultClass,
-            topic,
-            time,
-            results: 13,
-            cardData: [
-                {
-                    id: 1,
-                    title: 'title1',
-                    topics: 5,
-                    content: 'content1',
-                    time: 5,
-                    img: ''
-                },
-                {
-                    id: 2,
-                    title: 'title2',
-                    topics: 10,
-                    content: 'content2',
-                    time: 5,
-                    img: ''
-                },
-                {
-                    id: 2,
-                    title: 'title2',
-                    content: 'content2',
-                    topics: 15,
-                    time: 5,
-                    img: `https://scontent-tpe1-1.xx.fbcdn.net/v/t1.15752-9/101595173_237761764188877_5514067787007393792_n.jpg?_nc_cat=106&_nc_sid=b96e70&_nc_ohc=D8nl_qvFHqkAX8MmmRO&_nc_ht=scontent-tpe1-1.xx&oh=8c9a645782af594cbf5d2e4cb4e963de&oe=5EFB8570`
-                },
-                {
-                    id: 2,
-                    title: 'title2',
-                    content: 'content2',
-                    topics: 20,
-                    time: 4,
-                    img: `https://scontent-tpe1-1.xx.fbcdn.net/v/t1.15752-9/101595173_237761764188877_5514067787007393792_n.jpg?_nc_cat=106&_nc_sid=b96e70&_nc_ohc=D8nl_qvFHqkAX8MmmRO&_nc_ht=scontent-tpe1-1.xx&oh=8c9a645782af594cbf5d2e4cb4e963de&oe=5EFB8570`
-                }
-            ],
-            category: [
-                {
-
-                }
-            ]
+  name: "allclass",
+  components: {
+    LeftBar
+  },
+  data() {
+    return {
+      defaultClass,
+      topic,
+      time,
+      results: 13,
+      cardData: [
+        {
+          id: 1,
+          title: "title1",
+          topics: 5,
+          content: "content1",
+          time: 5,
+          img: ""
+        },
+        {
+          id: 2,
+          title: "title2",
+          topics: 10,
+          content: "content2",
+          time: 5,
+          img: ""
+        },
+        {
+          id: 2,
+          title: "英文課",
+          content: "content2",
+          topics: 15,
+          time: 5,
+          img:
+            "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/c6d42972507957.5be9f78763e91.jpg"
+        },
+        {
+          id: 2,
+          title: "title2",
+          content: "content2",
+          topics: 20,
+          time: 4,
+          img:
+            "https://www.planesandballoons.com/wp-content/uploads/2018/12/large-printable-numbers-1-20-02-255x329.png"
+        },
+        {
+          id: 2,
+          title: "title2",
+          content: "content2",
+          topics: 20,
+          time: 4,
+          img:
+            "https://www.planesandballoons.com/wp-content/uploads/2018/12/large-printable-numbers-1-20-02-255x329.png"
+        },
+        {
+          id: 2,
+          title: "title2",
+          content: "content2",
+          topics: 20,
+          time: 4,
+          img:
+            "https://www.planesandballoons.com/wp-content/uploads/2018/12/large-printable-numbers-1-20-02-255x329.png"
+        },
+        {
+          id: 2,
+          title: "title2",
+          content: "content2",
+          topics: 20,
+          time: 4,
+          img:
+            "https://www.planesandballoons.com/wp-content/uploads/2018/12/large-printable-numbers-1-20-02-255x329.png"
         }
-    },
-    methods: {
-        
-    }
-}
+      ],
+      category: [{}]
+    };
+  },
+  methods: {}
+};
 </script>
 <template lang="pug">
-    #allclass.allclass
-        LeftBar
-        .right
+    .allclass
+        LeftBar.leftbar
+        .classcard
             Col(v-for="(item, index) in cardData" :key='item.id')
-                Card.card(:boardered='11')
-                    img(v-if='item.img != "" ' :src="item.img" )
-                    img(v-else :src="defaultClass") 
+                Card.card
+                    .img
+                        img(v-if='item.img != "" ' :src="item.img" )
+                        img(v-else :src="defaultClass") 
+                    hr.hr
                     .title {{item.title}} 
                     .topics
                         img(:src='topic') 
@@ -80,33 +105,51 @@ export default {
 
 <style lang='scss' scoped>
 .allclass {
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  .leftbar {
+    flex: 1;
+  }
+  .classcard {
     display: flex;
-    justify-content: center;
-    text-align: center;
-    .right {
-        display: flex;
-        flex: 7;
-        margin: 50px;
-        .card{
-            flex: 1;
-            margin: 15px;
-            width: 200px;
-            height: 300px;
-            img{
-                width: 75px;
-            }
-            .title{
-                font-size: 20px;
-            }
-            .topics, .time{
-                img{
-                    width: 18px;
-                }
-                font-size: 10px;
-                text-align: left;
-                margin: 5px;
-            }
+    flex: 7;
+    margin: 50px;
+    flex-wrap: wrap;
+    .card {
+      flex: 1;
+      margin: 15px;
+      width: 250px;
+      height: 350px;
+      .img {
+        height: 110px;
+        line-height: 110px;
+        img {
+          width: 75px;
+          vertical-align: middle;
         }
+      }
+      .hr {
+        border-top: 1px solid #e8eaec;
+        margin: 10px;
+      }
+      .title {
+        font-size: 20px;
+        font-weight: bold;
+      }
+      .topics,
+      .time {
+        img {
+          width: 18px;
+          vertical-align: middle;
+          margin: 5px;
+        }
+        line-height: 18px;
+        font-size: 12px;
+        text-align: left;
+        margin: 5px;
+      }
     }
+  }
 }
 </style>
