f11ba337830b6dbc119c0b9a2a19b770636351c4
a685288 <a685288@gmail.com>
2020-07-14 13:53:26 +0800

    course info & topic

--- src/components/content/coursetopic.vue
+++ src/components/content/coursetopic.vue
@@ -1,15 +1,49 @@
-<template lang="pug">
-    .div
-        h1 包含的主題
-</template>
 <script>
 export default {
-  name: "coursetopic"
+  name: "coursetopic",
+  prop: {},
+  data() {
+    return {
+      Lessons: [
+        {
+          id: "1",
+          title: "英文的時間邏輯",
+          url: ""
+        },
+        {
+          id: "2",
+          title: "萬用句型",
+          url: ""
+        }
+      ]
+    };
+  }
 };
 </script>
+<template lang="pug">
+  .div
+    h1 包含的課程 
+    .lessons(v-for="(item, index) in Lessons",:key="item.id")
+      Card.card 第{{index+1}}課－{{item.title}}
+        Icon.icon(type="md-arrow-dropright-circle") 
+</template>
 <style lang='scss' scoped>
-.div {
-  height: 200px;
-  background-color: lawngreen;
+.div { 
+  background-color: lightgray;
+  padding: 5% 15%;
+  h1 {
+    text-align: left;
+    line-height: 50px;
+  }
+  .card {
+    font-size: 20px;
+    text-align: left;
+    margin: 1%;
+    padding: 0% 25%;
+    font-weight: bold;
+    .icon{
+      float: right;
+    }
+  }
 }
 </style>    
--- src/views/dashboardPages/Courseinfo.vue
+++ src/views/dashboardPages/Courseinfo.vue
@@ -1,26 +1,31 @@
 <script>
 import coursetopic from "@/components/content/coursetopic.vue";
 import topic from "@/assets/topic.png";
-import time from "@/assets/time.png";
 export default {
   name: "courseinfo",
-  comments: {
+  components: {
     coursetopic
   },
   data() {
     return {
       topic,
-      time
+      course: [
+        {
+          title: "英文基礎課",
+          content: "成就你的未來",
+          topic: "13"
+        }
+      ]
     };
   }
 };
 </script>
 <template lang="pug">
     .content
-        .info
+        .info(v-for="item in course",:key="item.id")
             .left
-                h1 英文基礎課
-                h3 成就你的未來
+                h1 {{item.title}}
+                h3 {{item.content}}
                 Button(type="primary",icon="md-arrow-round-forward") 開始上課
             Divider(type="vertical")
             .right
@@ -28,12 +33,12 @@ export default {
                     h3 課程進度
                     i-circle(:percent="80")
                         span.demo-Circle-inner 80%
-                .box  
+                .box
                     h3 課程資訊
                     img(:src='topic')
-                    | 主題：13  
-                    img(:src='time') 
-                    |時間（小時）：
+                    | 主題：{{item.topic}}
+                    icon(type='md-cloud-done')
+                    | 公開課程：{{item.topic}}
         coursetopic
 </template>
 <style lang="scss" scoped>
@@ -61,6 +66,9 @@ export default {
           width: 18px;
           vertical-align: middle;
           margin: 5px;
+        }
+        icon{
+          display: block;
         }
       }
     }
