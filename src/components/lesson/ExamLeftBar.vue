0cd0557bd7b4fbce9903032e4d99ecc17cd93b84
a685288 <a685288@gmail.com>
2020-10-13 16:57:34 +0800

    1013

--- src/components/lesson/ExamLeftBar.vue
+++ src/components/lesson/ExamLeftBar.vue
@@ -9,29 +9,15 @@ export default {
     return {
       theClassId: "",
       section: [
-        { sectionId: "5f797dd3048cab61b8da238f", sectionTitle: "gbgfn" },
-        { sectionId: "hrtjhytj", sectionTitle: "gbnhgnghhgfn" }
+        { sectionId: "",title: "" },
       ]
-
-      // lessons: [
-      //   {
-      //     id: "1",
-      //     title: "時間邏輯",
-      //     url: ""
-      //   },
-      //   {
-      //     id: "2",
-      //     title: "句型",
-      //     url: ""
-      // }
-      // ]
     };
   },
   mounted() {
     this.theClassId = this.$route.params.classId;
     console.log("ExamLeftBar-this.theClassId---" + this.theClassId);
     getCourseInfo(this.theClassId).then(res => {
-      this.section = res.data.data.section;
+      this.section = res.data.data.sections;
       // 須測試
     });
   },
@@ -45,7 +31,7 @@ export default {
 <template lang="pug">
 .div
   Menu(active-name="1")
-    MenuItem(:name="item.sectionId", v-for="(item,index) in lessons", :key="index",@click="toSection(item.sectionId)")
+    MenuItem(:name="item.sectionId", v-for="(item,index) in section", :key="index",@click="toSection(item.sectionId)")
       | L{{ index+1 }}－{{ item.title }}
 </template>
 <style lang="scss" scoped>
