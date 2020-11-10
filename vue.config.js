31f7fbe29f302a2ae788edbdf13317943e54549f
a685288 <a685288@gmail.com>
2020-11-10 10:53:18 +0800

    router index.js & vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,18 @@
+// const { turn } = require("core-js/fn/array");
+
 module.exports = {
-	// publicPath: "stuMode/",
+	publicPath: process.env.NODE_ENV ==='production'
+	? '/student/'
+	: './',
 	devServer: {
+		disableHostCheck:true,
+		https: true,
 		proxy: {
 			'/api':{
-				target: 'http://163.17.135.152:1314/',
+				// 測試用
+				// target: 'http://163.17.135.152:1314/',
+				// 上線用
+				target: 'http://127.0.0.1/',
 				changeOrigin: true,
 				pathRewrite: {
 					'^/api': '' 
