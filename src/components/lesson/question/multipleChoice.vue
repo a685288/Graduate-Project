561a38bd3af5dbd712a83fb215c6d03251f4331d
曾子銘 <zengziming@zengzimingdeMacBook-Pro.local>
2020-11-13 10:40:26 +0800

    multple

--- src/components/lesson/question/multipleChoice.vue
+++ src/components/lesson/question/multipleChoice.vue
@@ -36,27 +36,42 @@ export default {
         this.question.records.selects.push(e);
       });
     }
-    console.log(this.question.answer)
+
+    // console.log(this.question.records.selects)
+    // console.log(this.question.select)
+    console.log(this.question.answer);
   },
   methods: {
     ans() {
       this.userAns.questionId = this.question.questionId;
       this.$emit("emitAns", this.userAns);
+    },
+    // 判斷回傳是否該渲染打勾
+    answercheck(index) {
+      let ans = this.question.answer;
+      let search = ans.indexOf(index);
+      if (search === -1) {
+        return false;
+      }
+      return true;
     }
   }
 };
 </script>
 <template lang="pug">
 .div
-  .left(v-if='isAnswerData')
-    CheckboxGroup.CheckboxGroup(vertical, v-model="question.answer") 
-      h3 正確答案 
-      Checkbox.checkbox(v-for='(item,index) in question.select' :key='index' :label="index") {{ }}
+  //- .left(v-if='isAnswerData')
+  //-   CheckboxGroup.CheckboxGroup(vertical, v-model="question.answer") 
+  //-     h3 正確答案 
+  //-     Checkbox.checkbox(v-for='(item,index) in question.select' :key='index' :label="index") {{ }}
   .right
     //-做完畫面 
     CheckboxGroup.CheckboxGroup(v-if='question.records != undefined' vertical, v-model="question.records.selects")
       h3 {{ question.content }}
-      Checkbox.checkbox(v-for='(item, index) in question.select' :key='index' :label="index") {{ item }}
+      .asngroup(v-for='(item, index) in question.select')
+        Checkbox.checkbox(:key='index' :label="index") {{ item }}
+        icon.coverbox(type='ios-checkmark-circle' v-if="answercheck(index)" size = "22")
+        //- .coverbox(v-if="answercheck(index)")
     //- 作答中
     CheckboxGroup.CheckboxGroup(v-else vertical, v-model="userAns.selects", @on-change="ans()") 
       h3 {{ question.content }}
@@ -69,19 +84,17 @@ export default {
     margin-right: 7px;
     // display: inline-block;
     flex: 1;
-    .CheckboxGroup 
-    {background-color:#e6f7ff;
+    .CheckboxGroup {
+      background-color: #e6f7ff;
       .checkbox {
         display: block;
         width: 20px;
         margin: 10px auto;
       }
       h3 {
-  color: #003a8c  
-;
+        color: #003a8c;
+      }
     }
-    }
-    
   }
   .right {
     // display: inline-block;
@@ -90,8 +103,17 @@ export default {
     .CheckboxGroup {
       display: block;
       .checkbox {
+        float: left;
+        width: 95%;
         margin: 10px 0px;
+        border-bottom: 1px solid;;
         display: block;
+      }
+      .coverbox {
+        float: right;
+        color: rgb(91, 150, 2);
+        size:50px;
+        margin: 10px 0px;
       }
     }
   }
